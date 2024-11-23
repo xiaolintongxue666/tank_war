bfcf78909e8850e0ff2544dcd30de6c170b5d39b
mt <1139684993@qq.com>
2024-11-23 19:53:01 +0800

    "优化"

--- player.py
+++ player.py
@@ -2,7 +2,6 @@ from tank import Tank
 
 class Player:
     def __init__(self, name, controls, tank_image, initial_position):
-        super.__init__()
         self.name = name
         self.controls = controls
         self.tank = Tank(initial_position, tank_image, controls)
